2e47452a550d96bd0483f1880dd1cbcd2c7907db
Agathius <p.akos1148@gmail.com>
2023-01-02 12:54:44 +0100

    fixed api url

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const baseURL = 'https://task-manager-api.onrener.com/' //api base url hosted on render.com 
+const baseURL = 'https://task-manager-api-ywz0.onrender.com' //api base url hosted on render.com 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
   "/api/v1/tasks": {
-    target: baseURL, // 'http://localhost:3000/'
+    target: baseURL, //'http://localhost:3000/'
     changeOrigin: true,
     secure: false,
     ws: true
